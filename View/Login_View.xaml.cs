58f13567044fc5320627a5bd2c62b92961b94641
Crisostomo, Lance M. <lance.crisostomo235@gmail.com>
2025-10-15 09:47:43 +0800

    Fixed some issues

--- View/Login_View.xaml.cs
+++ View/Login_View.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using StudentViolationApplication.ViewModel;
 
 namespace StudentViolationApplication.View
 {
@@ -21,6 +22,7 @@ namespace StudentViolationApplication.View
     {
         public Login_View()
         {
+            DataContext = new LoginViewModel();
             InitializeComponent();
         }
 
